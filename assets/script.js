e1fd1695331c2f1338f197129609532acb5a9f1a
Ayush <ayushmanisharma@gmail.com>
2023-06-12 19:00:17 -0400

    Implemented basic MediWiki API

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,22 @@ function getSearch() {
     }
 };
 
+function fetchWikiExtract(){
+    const wikiEndpoint = 'https://simple.wikipedia.org/w/api.php';
+    const wikiParams = '?action-query'
+    + "&prop=extracts" //an'extract' is the type of property being used
+    + "&exlimit=1" //idk
+    + "&titles=" + "" //tells the link which specific wikipedia page to get an extract from (changes based on the 'ele' param)
+    + "&explaintext=1" //tells the API to provide the content in plain text (instead of html code or other formats)
+    + "&format=json" //requests data in Json format
+    + "&formatversion=2" //makes the JSON properties easier to navigate using dot notation
+    + "&origin=*" //omitting this param causes CORS error
+    ;
+
+    const wikiLink = wikiEndpoint + wikiParams;
+    console.log(wikiLink);
+}
+
 //use getSearch() to generate array of info for search result
 function searchArray(getSearch){
     var name = this.name;

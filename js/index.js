d29b32b5511df561d8440b583aba828318a493d6
SMarquis85 <sophienathalie_85@outlook.com>
2023-06-18 13:23:33 -0400

    updated js files

---
+++ js/index.js
@@ -0,0 +1,148 @@
+var script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCLsgBkf2kPSWIYMK0fU7VFxf1p-5Rexi8&callback=initMap';
+script.defer = true;
+document.head.appendChild(script);
+var key = "AIzaSyCLsgBkf2kPSWIYMK0fU7VFxf1p-5Rexi8";
+
+window.onload = () => {
+  displayStores(stores);
+};
+
+var map;
+var markers = [];
+var infoWindow;
+
+function initMap() {
+  var ottawa = {
+    lat: 45.4215,
+    lng: -75.6981,
+  };
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: ottawa,
+    zoom: 11,
+    mapTypeId: 'roadmap',
+  });
+  infoWindow = new google.maps.InfoWindow();
+  searchStores();
+}
+
+const searchStores = () => {
+  let foundStores = [];
+  let postalCode = document.getElementById('zipcode-input').value;
+  if (postalCode) {
+    stores.forEach((store) => {
+      let storePostalCode = store.address.postalCode;
+      if (storePostalCode.startsWith(postalCode)) {
+        foundStores.push(store);
+      }
+    });
+  } else {
+    foundStores = stores;
+  }
+  if (foundStores.length === 0) {
+    foundStores = stores;
+  }
+  clearLocation();
+  displayStores(foundStores);
+  showStoresMarker(foundStores);
+  setOnClickListener();
+};
+
+const clearLocation = () => {
+  infoWindow.close();
+  markers.forEach((marker) => {
+    marker.setMap(null);
+  });
+  markers.length = 0;
+};
+
+const setOnClickListener = () => {
+  let storeElements = document.querySelectorAll('.stores-container');
+  storeElements.forEach((element, index) => {
+    element.addEventListener('click', () => {
+      google.maps.event.trigger(markers[index], 'click');
+    });
+  });
+};
+
+const displayStores = (stores) => {
+  let storesHTML = '';
+  stores.forEach((store, count) => {
+    storesHTML += `
+      <div class="stores-container">
+        <div class="store-container-background">
+          <div class="store-info-container">
+            <div class="store-address">
+              <span>${store.addressLines[0]}</span>
+              <span>${store.addressLines[1]}</span>
+            </div>
+            <div class="store-phone-number">${store.phoneNumber}</div>
+          </div>
+          <div class="store-number-container">
+            <div class="store-number">${count + 1}</div>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+  document.querySelector('.stores-list').innerHTML = storesHTML;
+};
+
+const showStoresMarker = (stores) => {
+  var bounds = new google.maps.LatLngBounds();
+  stores.forEach((store, count) => {
+    let latlng = new google.maps.LatLng(
+      store.coordinates.latitude,
+      store.coordinates.longitude
+    );
+    let name = store.name;
+    let address = store.addressLines[0];
+    let openStatus = store.openStatusText;
+    let phoneNumber = store.phoneNumber;
+    bounds.extend(latlng);
+    createMarker(latlng, name, address, openStatus, phoneNumber, count + 1);
+  });
+  map.fitBounds(bounds);
+};
+
+const createMarker = (
+  latlng,
+  name,
+  address,
+  openStatus,
+  phoneNumber,
+  count
+) => {
+  var html = `
+    <div class="store-info-window">
+      <div class="store-info-name">
+        ${name}
+      </div>
+      <div class="store-info-status">
+        ${openStatus}
+      </div>
+      <div class="store-info-address">
+        <div class="circle">
+          <i class="fas fa-location-arrow"></i>
+        </div>
+        ${address}
+      </div>
+      <div class="store-info-phone">
+        <div class="circle">
+          <i class="fas fa-phone-alt"></i>
+        </div>
+        ${phoneNumber}
+      </div>
+    </div>
+  `;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: latlng,
+    label: count.toString(),
+  });
+  google.maps.event.addListener(marker, 'click', function () {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+  });
+  markers.push(marker);
+};
